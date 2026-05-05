f993f76c85f79a118533e922384a0971c6f9a682
Rango ! <baloney7987@gmail.com>
2026-05-04 22:33:57 -0400

    Update throttle.py

--- utsm_telemetry/throttle.py
+++ utsm_telemetry/throttle.py
@@ -1,1 +1,17 @@
 ## WORK IN PROGRESS!
+
+## Useful output columns:
+
+throttle_bin_lo
+throttle_bin_hi
+sample_count
+avg_current_mA
+avg_power_w
+avg_gps_accel_m_s2
+avg_imu_dynamic_accel_m_s2
+avg_speed_kph
+energy_wh
+distance_m
+efficiency_wh_per_km
+
+def build_throttle_summary():
